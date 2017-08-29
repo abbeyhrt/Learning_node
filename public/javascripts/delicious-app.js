077259b63324eae18f955254d6655e4ccd99cacc
Abbey Hart <abbeyhrt@gmail.com>
2017-08-29 16:25:47 -0400

    Change geolocator

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+
+// because of modules/bling $ can be used in the place of document.querySelector because that method was made in that module and then we imported it onto this page
+autocomplete($('#address'), $('#lat'), $('#lng'));
