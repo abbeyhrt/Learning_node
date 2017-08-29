ab8ca11fe6069ef1864a666065f8a78c449deb81
Abbey Hart <abbeyhrt@gmail.com>
2017-08-29 14:17:05 -0400

    Change store form to include address

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -33,3 +33,15 @@ exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return thupdatese new store instead of old store
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store -></a> `
+  );
+  res.redirect(`/stores/${store._id}/edit`);
+};
