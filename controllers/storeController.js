a551d568b15b29bf84616d875fcd6a70de93984f
Abbey Hart <abbeyhrt@gmail.com>
2017-08-31 15:15:49 -0400

    add image uploading capabilities

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That file type is not allowed!' }, false);
+    }
+  },
+};
 
 exports.homePage = (req, res) => {
   req.flash('error', 'Something Happened');
@@ -13,7 +28,24 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add store' });
 };
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is not new file to resize
+  if (!req.file) {
+    next(); //skip to next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+};
+
 exports.createStore = async (req, res) => {
+  // multer allows for uploading a fi
   const store = new Store(req.body);
   await store.save();
   req.flash(
@@ -29,12 +61,21 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) {
+    return next();
+  }
+  res.render('store', { store, title: store.name });
+};
+
 exports.editStore = async (req, res) => {
   const store = await Store.findOne({ _id: req.params.id });
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return thupdatese new store instead of old store
     runValidators: true,
